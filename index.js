b92ba9c3423289f40ce075e7db522e3350bf2df5
@nain333 <hnain70@gmail.com>
2023-08-12 21:30:56 +0530

    set up views and layout for the application

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 9000
+const expressLayouts=require('express-ejs-layouts')
 const db = require('./config/mongoose.js')
 app.set('view engine','ejs')
+app.use(expressLayouts)
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
 app.use('/',require('./routes'))
 app.listen(port,(err)=>{
     if(err){
